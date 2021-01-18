13cc183f1009f849aedd0dd52f34e56d8a98e94f
Phil Haines <phil.haines92@gmail.com>
2021-01-18 02:30:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -187,7 +187,7 @@ end
 def player_stats(name)
   game_hash.each do |key, value|
     value.each do |inner_hash|
-      inner_hash[:player_name] = name 
+      inner_hash[:player_name]
       binding.pry
         if inner_hash[:player_name] == name
           return inner_hash
