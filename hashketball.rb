c63a3bf6a79f26f92d7a4431793d9aa7906990b3
Phil Haines <phil.haines92@gmail.com>
2021-01-18 02:49:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,21 +194,6 @@ def player_stats(name)
   end
 end
 
-def most_points_scored
-  most_points = 0
-  high_scorer = ""
-  game_hash.each do |key, value|
-    value[:players].each do |inner_hash|
-      if inner_hash[:points] > most_points
-        most_points == inner_hash[:points]
-        high_scorer == inner_hash[:player_name]
-      end
-    end
-  end
-  binding.pry
-  return high_scorer
-end
-
 
 def big_shoe_rebounds
   largest_shoe_size = 0
@@ -225,3 +210,16 @@ def big_shoe_rebounds
 end
 
 
+def most_points_scored
+  most_points = 0
+  high_scorer = ""
+  game_hash.each do |key, value|
+    value[:players].each do |inner_hash|
+      if inner_hash[:points] > most_points
+        most_points == inner_hash[:points]
+        high_scorer == inner_hash[:player_name]
+      end
+    end
+  end
+  return high_scorer
+end
